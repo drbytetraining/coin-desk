24c3f6f7fda789f3d62dd6e1ed9ad2cce23c9503
mawais8086 <mawaismaghfoor@gmail.com>
2024-03-21 16:03:48 +0500

    comment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const commentController = require('../controllers/commentController');
 
 // authentication routes
 router.post('/register', authController.register);
@@ -12,21 +13,31 @@ router.get('/refresh',auth,  authController.logout);
 
 // post/ blog
 
-// create 
-router.post('/blog', auth)
+// // create 
+// router.post('/blog', auth)
 
-// get all
-router.get('/blog/all', auth)
+// // get all
+// router.get('/blog/all', auth)
 
-// get specific
-router.get('/blog/:id', auth)
+// // get specific
+// router.get('/blog/:id', auth)
 
-// update blog
-router.put('/blog/:id', auth)
+// // update blog
+// router.put('/blog/:id', auth)
 
-// delete
-router.delete('/blog/:id', auth)
+// // delete
+// router.delete('/blog/:id', auth)
 
 
+// base64 
+// then store into db 
+
+
+
+// comments
+//create
+router.post('/comment', commentController.create);
+
+router.get('/comment/:id', commentController.getById)
 
 module.exports =router
